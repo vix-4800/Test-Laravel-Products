de650a2d50a05cf744a7cf1950d2c3974b50edee
Anton Vix <Gordan.ei@protonmail.ch>
2025-02-22 07:47:31 +0400

    product index page update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
     public function index(): View
     {
-        return view('products.index');
+        return view('products.index', [
+            'products' => Product::with('category')->get(),
+        ]);
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,4 +4,51 @@
             {{ __('Products') }}
         </h2>
     </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6  text-gray-900 dark:text-gray-100 font-sans overflow-x-auto">
+                    <table class="w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-100 whitespace-nowrap">
+                            <tr>
+                                <th class="px-4 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th class="px-4 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                                    Price
+                                </th>
+                                <th class="px-4 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                                    Category
+                                </th>
+                                <th class="px-4 py-4 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                                    Actions
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody class="bg-white divide-y divide-gray-200 whitespace-nowrap">
+                            @foreach($products as $product)
+                            <tr>
+                                <td class="px-4 py-4 text-sm text-gray-800">
+                                    {{ $product->name }}
+                                </td>
+                                <td class="px-4 py-4 text-sm text-gray-800">
+                                    {{ $product->price }}
+                                </td>
+                                <td class="px-4 py-4 text-sm text-gray-800">
+                                    {{ $product->category->name }}
+                                </td>
+                                <td class="px-4 py-4 text-sm text-gray-800">
+                                    <button class="text-blue-600 mr-4">Edit</button>
+                                    <button class="text-red-600">Delete</button>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
